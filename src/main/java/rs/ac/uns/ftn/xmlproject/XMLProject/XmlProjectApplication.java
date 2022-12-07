60753935355e82f2fab7395f612e4bb1e8a92910
ikkjo <ilija.kalinic@gmail.com>
2022-12-07 17:16:04 +0100

    Adds spring project and dependencies

---
+++ src/main/java/rs/ac/uns/ftn/xmlproject/XMLProject/XmlProjectApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.xmlproject.XMLProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/ac/uns/ftn/xmlproject/XMLProject/XmlProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.xmlproject.XMLProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
