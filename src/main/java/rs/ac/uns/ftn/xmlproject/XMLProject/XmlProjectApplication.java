f58c190b6fa55e995264ddabdb8a9c59bad48385
ikkjo <ilija.kalinic@gmail.com>
2022-12-12 10:13:48 +0100

    Removes database and java jpa libs from pom.xml

--- src/main/java/rs/ac/uns/ftn/xmlproject/XMLProject/XmlProjectApplication.java
+++ src/main/java/rs/ac/uns/ftn/xmlproject/XMLProject/XmlProjectApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class XmlProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XmlProjectApplication.class, args);
+		// ova linija ce trebati kasnije
+		// SpringApplication.run(XmlProjectApplication.class, args);
 	}
 
 }
