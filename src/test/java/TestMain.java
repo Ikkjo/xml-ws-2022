d2d6ba3fd6cf1a34d447843a14985380eee43c58
Saska Topalovic <tsaska98@gmail.com>
2023-01-11 15:08:43 +0100

    Update p-1 marshalling/unmarshalling

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,13 +1,15 @@
 import xml.ATest;
+import xml.PTest;
 
 public class TestMain {
 
     private static final String ATestFilepath = "xml/a-1_instance1.xml";
+    private static final String PTestFilePath = "xml/p-1_instance.xml";
 
     public static void main(String[] args) {
 
-        ATest.test(ATestFilepath);
-
+        //ATest.test(ATestFilepath);
+        PTest.test(PTestFilePath);
     }
 
 }
--- src/test/java/xml/PTest.java
+++ src/test/java/xml/PTest.java
@@ -1,38 +1,35 @@
 package xml;
 
-import models.p.Address;
-import models.p.RequestForPatentRecognition;
+import models.p.*;
 
-import javax.xml.XMLConstants;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.math.BigInteger;
 
 public class PTest {
-    public static void test() {
+    public static void test(String testFilePath) {
         try {
             System.out.println("[INFO] P-1 unmarshalling example.\n");
 
             // Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
             JAXBContext context = JAXBContext.newInstance("models.p");
+
             // Unmarshaller je objekat zadužen za konverziju iz XML-a u objektni model
             Unmarshaller unmarshaller = context.createUnmarshaller();
 
-            // XML schema validacija
-            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = schemaFactory.newSchema(new File("./xml/schemes/p-1.xsd"));
+            // Unmarshalling generiše objektni model na osnovu XML fajla
+            RequestForPatentRecognition request = (RequestForPatentRecognition) unmarshaller.unmarshal(new File(testFilePath));
 
-            // Podešavanje unmarshaller-a za XML schema validaciju
-            unmarshaller.setSchema(schema);
-            unmarshaller.setEventHandler(new MyValidationEventHandler());
+            // Prikazuje unmarshallovan objekat
+            printRequest(request);
 
-            // Test: proširiti XML fajl nepostojećim elementom (npr. <test></test>)
-            RequestForPatentRecognition request = (RequestForPatentRecognition) unmarshaller.unmarshal(new File("./xml/schemes/p-1.xsd"));
+            // Promeni vrednosti
+            request.getInformationForInstitution().setApplicationNumber("P-9999");
+            request.getPatentInformation().getInventor().setEmail("zikazika12@gmail.com");
 
             // Marshaller je objekat zadužen za konverziju iz objektnog u XML model
             Marshaller marshaller = context.createMarshaller();
@@ -41,36 +38,55 @@ public class PTest {
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
             // Ispis sadržaja objektnog modela, nakon uspešne validacije
-            marshaller.marshal(request, System.out);
+            marshaller.marshal(request, new FileOutputStream("src/test/data/p_new.xml"));
 
-            // Izmena dokumenta
-            request.setProxy(newProxy());
-
-            // Cuvanje izmena dokumenta
-            marshaller.marshal(request, new FileOutputStream("./xml/p-1_output.xml"));
-
-        } catch (Exception e) {
+        } catch (JAXBException | FileNotFoundException e) {
             e.printStackTrace();
+            System.out.println("[ERROR] Test failed.");
         }
     }
 
-    private static RequestForPatentRecognition.Proxy newProxy() {
+    private static void printRequest(RequestForPatentRecognition request) {
+        // Prikaz broja prijave i datuma podnosenja privaje
+        System.out.println("ZAHTEV ZA PRIZNANJE PATENTA\n");
+        System.out.printf(
+                "Broj prijave: %s\nDatum prijema: %s\t\tPriznati datum podnosenja:%s%n\n",
+                request.getInformationForInstitution().getApplicationNumber(),
+                request.getInformationForInstitution().getReceiptDate(),
+                request.getInformationForInstitution().getSubmissionDate()
+        );
 
-        RequestForPatentRecognition.Proxy newProxy = new RequestForPatentRecognition.Proxy();
+        // Request
+        PatentInformation.Applicant requestApplicant = request.getPatentInformation().getApplicant();
+        System.out.println("PODNOSILAC");
+        System.out.printf("Adresa:\n\tGrad: %s\n\tUlica: %s\n\tPostanski broj: %s\n",
+                requestApplicant.getAddress().getCity(),
+                requestApplicant.getAddress().getStreet() + requestApplicant.getAddress().getStreetNumber(),
+                requestApplicant.getAddress().getZipCode());
+        System.out.printf("Broj telefona: %s\n", requestApplicant.getPhoneNumber());
+        System.out.printf("E-posta: %s\n", requestApplicant.getEmail());
+        System.out.printf("Broj faksa: %s\n", requestApplicant.getFaxNumber());
+        TPerson requestInventor = request.getPatentInformation().getInventor();
+        if (requestInventor instanceof TIndividual) {
+            TIndividual applicantIndividual = (TIndividual) requestInventor;
+            System.out.println("\nPRONALAZAC");
+            System.out.printf("Ime: %s\n", applicantIndividual.getFirstName());
+            System.out.printf("Prezime: %s\n", applicantIndividual.getLastName());
+            System.out.printf(
+                    "Adresa:\n\tGrad: %s\n\tUlica: %s\n\tPostanski broj: %s\n",
+                    applicantIndividual.getAddress().getCity(),
+                    applicantIndividual.getAddress().getStreet() + applicantIndividual.getAddress().getStreetNumber(),
+                    applicantIndividual.getAddress().getZipCode()
+            );
+            String citizenship = applicantIndividual.getCitizenship();
 
-        newProxy.setAddress(new Address());
-        newProxy.getAddress().setStreet("Tolstojeva");
-        newProxy.getAddress().setStreetNumber(BigInteger.valueOf(40));
-        newProxy.getAddress().setCity("Novi Sad");
-        newProxy.getAddress().setZipCode(21000);
-        newProxy.setPhoneNumber("0645050888");
-        newProxy.setFirstName("Pera");
-        newProxy.setLastName("Peric");
+            if (citizenship != null) {
+                System.out.printf("\tDrzavljanstvo: %s\n", citizenship);
+            } else {
+                System.out.println("\tDrzavljanstvo: Nije naznaceno");
+            }
 
-        return newProxy;
-    }
 
-    public static void main(String[] args) {
-        test();
+        }
     }
 }
