b79b66939293fac88f7fe29fea6ab5663adfc62f
ikkjo <ilija.kalinic@gmail.com>
2023-01-27 17:17:00 +0100

    Adds app routing module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CopyrightRequestFormComponent } from './forms/copyright-request-form/copyright-request-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CopyrightRequestFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
