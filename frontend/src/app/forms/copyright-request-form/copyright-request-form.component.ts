47553a261c65be47ac24a23f82ebf80008f61058
ikkjo <ilija.kalinic@gmail.com>
2023-01-28 17:11:04 +0100

    Finishes copyright request form

--- frontend/src/app/forms/copyright-request-form/copyright-request-form.component.ts
+++ frontend/src/app/forms/copyright-request-form/copyright-request-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 interface PersonType {
   value: string;
@@ -14,19 +14,66 @@ interface PersonType {
 
 export class CopyrightRequestFormComponent implements OnInit {
 
-  copyrightRequestForm: any;
+  public copyrightRequestForm!: FormGroup;
+
+  public isAuthorAnonymous: boolean = false;
+
+  ngOnInit() {
+
+    this.copyrightRequestForm = this.fb.group({
+      applicantBusinessName: ['', Validators.required],
+      applicantName: ['', Validators.required],
+      applicantSurname: ['', Validators.required],
+      applicantEmail: ['', Validators.required, Validators.email],
+      applicantPhoneNumber: ['', Validators.required],
+      applicantStreet: ['', Validators.required],
+      applicantStreetNumber: ['', Validators.required],
+      applicantZipCode: ['', Validators.required],
+      applicantCity: ['', Validators.required],
+      attorneyName: ['', Validators.required],
+      attorneySurname: ['', Validators.required],
+      attorneyEmail: ['', Validators.required, Validators.email],
+      attorneyPhoneNumber: ['', Validators.required],
+      attorneyStreet: ['', Validators.required],
+      attorneyStreetNumber: ['', Validators.required],
+      attorneyZipCode: ['', Validators.required],
+      attorneyCity: ['', Validators.required],
+      isAuthorApplicant: [false],
+      authorName: [''],
+      authorSurname: [''],
+      authorAnonymous: [false],
+      authorPseudonym: [''],
+      authorDateOfDeath: [null],
+      workMainTitle: ['', Validators.required],
+      workAlternativeTitle: [''],
+      workFormOfRecording: ['', ],
+      workType: ['', ],
+      workWayOfUsing: ['', ],
+      workMadeInBusinessRelationship: [false, ],
+      originalWorkMainTitle: [''],
+      originalWorkAlternativeTitle: [''],
+      originalAuthorName: [''],
+      originalAuthorSurname: [''],
+      originalAuthorAnonymous: [false],
+      originalAuthorPseudonym: [''],
+      originalAuthorDateOfDeath: [null],
+      derivativeWork: [false],
+      applicantType: [''],
+    });
+  }
 
   types: PersonType[] = [
     {value: 'individual', viewValue: 'Физичко лице'},
     {value: 'legal', viewValue: 'Правно лице'},
   ];
 
-  constructor(private fb: FormBuilder) { };
+  submitForm() {
+    // TODO: send post request to server
 
-  ngOnInit() {
-    this.copyrightRequestForm = this.fb.group({
-      applicantEmail: ''
-    });
+    console.log(JSON.stringify(this.copyrightRequestForm));
   }
 
+  constructor(private fb: FormBuilder) {};
+
+  
 }
