a5c2bb74ecea33839f6c109e286dabbf0dac89c6
Saska Topalovic <tsaska98@gmail.com>
2023-02-01 22:03:50 +0100

    Adds system user DTO mapper

---
+++ authorization/src/main/java/backend/authorization/util/UserDTOMapper.java
@@ -0,0 +1,29 @@
+package backend.authorization.util;
+
+import backend.authorization.dto.RegistrationDTO;
+import backend.authorization.dto.SystemUserDTO;
+import backend.authorization.model.SystemUser;
+
+public class UserDTOMapper {
+
+    public SystemUser convertFromDTO(RegistrationDTO registrationDTO) {
+        SystemUser user = new SystemUser();
+
+        user.setFirstName(registrationDTO.firstName);
+        user.setLastName(registrationDTO.lastName);
+        user.setEmail(registrationDTO.email);
+        user.setPassword(registrationDTO.password);
+        user.setRole(registrationDTO.role);
+        return user;
+    }
+
+    public SystemUserDTO convertToDTO(SystemUser user) {
+        SystemUserDTO userDTO = new SystemUserDTO();
+
+        userDTO.firstName = user.getFirstName();
+        userDTO.lastName = user.getLastName();
+        userDTO.email = user.getEmail();
+        userDTO.role = user.getRole();
+        return userDTO;
+    }
+}
